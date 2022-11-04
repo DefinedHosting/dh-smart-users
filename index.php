2e5dd3a8e53c8492dcc9295294bd237d45954a92
Defined Hosting <info@definedhosting.co.uk>
2022-11-04 16:13:01 +0000

    1.0.1

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Defined Hosting | Smart Users
+Plugin URI: http://www.definedhosting.co.uk/plugins
+Description: User management for Smart Numbers
+Author: R. Cush
+Version: 1.0.1
+Author URI: https://www.definedhosting.co.uk
+GitHub Plugin URI: DefinedHosting/dh-smart-users
+GitHub Plugin URI: https://github.com/DefinedHosting/dh-smart-users
+*/
+
+include_once('dh-smart-users.php');
+register_activation_hook( __FILE__, 'dh_su_plugin_activated' );
+add_action( 'upgrader_process_complete', 'dh_su_plugin_update',10, 2);
